2375fda3946ad4395ad06013cbe4367973c5f657
zhouyanming <18152007693@163.com>
2025-05-09 15:12:50 +0800

    接口上下游传参参数化

--- test_manager/templatetags/custom_filters.py
+++ test_manager/templatetags/custom_filters.py
@@ -1,11 +1,37 @@
 from django import template
+import json
 
 register = template.Library()
 
 
 @register.filter
+def pprint(value):
+    """
+    Pretty print JSON or dict objects
+    """
+    if isinstance(value, str):
+        try:
+            value = json.loads(value)
+        except:
+            pass
+
+    if isinstance(value, (dict, list)):
+        return json.dumps(value, indent=2, ensure_ascii=False)
+    return value
+
+
+@register.filter
 def get_item(dictionary, key):
-    """获取字典中的项"""
-    if dictionary is None:
+    """
+    Get an item from a dictionary using the key
+    """
+    if not dictionary:
         return None
+
+    if isinstance(dictionary, str):
+        try:
+            dictionary = json.loads(dictionary)
+        except:
+            return None
+
     return dictionary.get(key)
