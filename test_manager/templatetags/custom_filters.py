643842ad8c3d317e1d5843d659c8832431a93813
zhouyanming <18152007693@163.com>
2025-05-13 11:40:25 +0800

    模版中文显示问题

--- test_manager/templatetags/custom_filters.py
+++ test_manager/templatetags/custom_filters.py
@@ -8,28 +8,34 @@ register = template.Library()
 
 @register.filter
 def pprint(value):
-    """Pretty print a JSON or dict object"""
+    """
+    Pretty print JSON or dict objects
+    """
     if isinstance(value, str):
         try:
-            # Try to parse as JSON
-            parsed = json.loads(value)
-            return json.dumps(parsed, indent=2, sort_keys=True)
-        except (ValueError, TypeError):
-            # If not JSON, return as is
-            return value
-    elif isinstance(value, (dict, list)):
-        # If already a dict or list, pretty print it
-        return json.dumps(value, indent=2, sort_keys=True)
-    else:
-        # For other types, use Python's pprint
-        return pprint.pformat(value)
+            value = json.loads(value)
+        except:
+            pass
+
+    if isinstance(value, (dict, list)):
+        return json.dumps(value, indent=2, ensure_ascii=False)
+    return value
 
 
 @register.filter
 def get_item(dictionary, key):
-    """Get an item from a dictionary using a key"""
-    if dictionary is None:
+    """
+    Get an item from a dictionary using the key
+    """
+    if not dictionary:
         return None
+
+    if isinstance(dictionary, str):
+        try:
+            dictionary = json.loads(dictionary)
+        except:
+            return None
+
     return dictionary.get(key)
 
 
